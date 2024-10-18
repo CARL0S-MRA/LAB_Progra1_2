5fe007cc4f1f8b72292ccb0ca3c0d7585b0c65ea
carlo <carlo@PCdeCarlosM>
2024-10-18 10:26:31 -0600

    Programa de calculacion de precio por cada Clics

---
+++ src/labprogra1_round2_Semana1/Carlos_Rodriguez_Clicks.java
@@ -0,0 +1,48 @@
+
+package labprogra1_round2_Semana1;
+import java.util.Scanner;
+public class Carlos_Rodriguez_Clicks {
+    public static void main(String[] args) {
+        Scanner lea=new Scanner(System.in);
+        int clicsTotales;
+        
+        int clic1;
+        double precioClic1;
+        
+        int clic2;
+        double precioClic2;
+        
+        int clic3;
+        double precioClic3;
+        
+        double totalPagar=0;
+        double ISV=1.16;
+        double CPC=0;
+        double totalISV=0;
+        
+        System.out.println("Ingrese los clics totales: ");
+        clicsTotales=lea.nextInt();
+        
+        System.out.println("Ingresar primeros clics: ");
+        clic1=lea.nextInt();
+        System.out.println("Precio de primeros clics: ");
+        precioClic1=lea.nextDouble();
+        
+        System.out.println("Ingresar segundos clics: ");
+        clic2=lea.nextInt();
+        System.out.println("Precio primeros clics: ");
+        precioClic2=lea.nextDouble();
+        
+        System.out.println("Ingresar terceros clics: ");
+        clic3=lea.nextInt();
+        System.out.println("Precio terceros cliscs: ");
+        precioClic3=lea.nextDouble();
+        
+        totalPagar = precioClic1*clic1+precioClic2*clic2+precioClic3*clic3;
+        CPC = totalPagar/clicsTotales;
+        totalISV = totalPagar * ISV;
+        
+        System.out.println(String.format(" %.2f", CPC));
+        System.out.println(String.format(" %.2f", totalISV));
+    }
+}
